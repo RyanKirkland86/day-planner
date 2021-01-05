9a020f20a0f61a3c5eb7d2398da180e28099ee8c
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-01-05 10:49:30 -0800

    Adding pseudocode for time block styling and time referencing

--- dayplanner.js
+++ dayplanner.js
@@ -1,6 +1,16 @@
+var plansText = "";
+var plansTime = "";
+
 $(document).ready(function () {
 
     console.log(moment());
 
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    
+//We need to right a function that checks the current hour of the day, and compares it to the chart.
+//If the hour of the day is greater than the hour shown, add the attribute of past to the time block
+//If the hour of the day matches the hour shown, add the attribute of present to the time block.
+//If the hour of the day is less than the hour shwon, add the attribute of future to the time block.
+    
+    console.log(moment().format("H"));
 })
