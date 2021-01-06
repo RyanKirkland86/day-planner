9fa1a21d213ac3405e1095dd866c7ed91db8c6aa
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-01-05 21:15:19 -0800

    Changed ID tags again back to the parent div, thinking this will help when I attempt to store the text value upon button click

--- dayplanner.js
+++ dayplanner.js
@@ -19,13 +19,13 @@ $(document).ready(function () {
         timeBlock = i;
 //Write conditionals to add style attributes
         if (currentHour > i){
-            $('#' + timeBlock).addClass("past");
+            $('#' + timeBlock).children('div').children("textarea").addClass("past")
         }
         else if (currentHour < i){
-            $('#' + timeBlock).addClass("future");
+            $('#' + timeBlock).children('div').children("textarea").addClass("future");
         }
         else {
-            $('#' + timeBlock).addClass("present");
+            $('#' + timeBlock).children('div').children("textarea").addClass("present");
         }
     }
 })
