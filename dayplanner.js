f40399d4485880afcf7b35de04fe666a1260f277
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-01-05 23:59:59 -0800

    Added code to capture the ID tag upon save click, checked with console log

--- dayplanner.js
+++ dayplanner.js
@@ -21,13 +21,13 @@ $(document).ready(function () {
         timeBlock = i;
 //Write conditionals to add style attributes
         if (currentHour > i){
-            $('#' + timeBlock).children('div').children("textarea").addClass("past")
+            $('#' + timeBlock).children("div").children("textarea").addClass("past")
         }
         else if (currentHour < i){
-            $('#' + timeBlock).children('div').children("textarea").addClass("future");
+            $('#' + timeBlock).children("div").children("textarea").addClass("future");
         }
         else {
-            $('#' + timeBlock).children('div').children("textarea").addClass("present");
+            $('#' + timeBlock).children("div").children("textarea").addClass("present");
         }
     }
 })
@@ -41,12 +41,14 @@ $(".saveBtn").on("click", function() {
         The event listener will capture the user input in the text field.
         We will use jquery parent/children documentation to target the textarea.
         We need to be able to save this data so we can push it to local storage. */
-    userInput = $(this).parent('div').children('textarea').val();
+    userInput = $(this).parent("div").children("textarea").val();
     console.log(userInput);
 /*    What? How Why?
         The event listener will also record the ID tag with the user input.
         We will use the same parent/children documentation to target the ID.
         We need to be able to pair the user text input with it's proper ID tag. */
+    userTime = $(this).parent("div").parent().attr("id");
+    console.log(userTime);
 /*    What? How? Why?
         We will take these two captured values and pair them together.
         Using a key/value array.
