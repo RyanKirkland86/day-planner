46973997f2e2ff417d743ddf9ecc6d6c4fc6c9cf
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-03-18 03:26:40 -0700

    Finalized the local storage code

--- dayplanner.js
+++ dayplanner.js
@@ -4,23 +4,25 @@ var userInput;
 var userTime;
 var userPlans = [];
 
-$(document).ready(function () {
+$(".nine").text(localStorage.getItem("9"));
+$(".ten").text(localStorage.getItem("10"));
+$(".eleven").text(localStorage.getItem("11"));
+$(".twelve").text(localStorage.getItem("12"));
+$(".one").text(localStorage.getItem("13"));
+$(".two").text(localStorage.getItem("14"));
+$(".three").text(localStorage.getItem("15"));
+$(".four").text(localStorage.getItem("16"));
+$(".five").text(localStorage.getItem("17"));
 
-    console.log(moment());
+$(document).ready(function () {
 
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
     
-//We need to write a function that checks the current hour of the day, and compares it to the chart.
-//If the hour of the day is greater than the hour shown, add the attribute of past to the time block
-//If the hour of the day matches the hour shown, add the attribute of present to the time block.
-//If the hour of the day is less than the hour shown, add the attribute of future to the time block.
-    
-    console.log(moment().format("H"));
     currentHour = (moment().format("H"));
-//Write a for loop that checks each block of time, with it's ID to the current Hour
+
     for (var i = 0; i <= 23; i++){
         timeBlock = i;
-//Write conditionals to add style attributes
+
         if (currentHour > i){
             $('#' + timeBlock).children("div").children("textarea").addClass("past")
         }
@@ -33,42 +35,46 @@ $(document).ready(function () {
     }
 })
 
-/*  What? How? Why?
-        We need to create an event listener for the save button.
-        We will use jquery.
-        To capture information that we want to save. */
+// This is what was working before:
 $(".saveBtn").on("click", function() {
-/*    What? How? Why?
-        The event listener will capture the user input in the text field.
-        We will use jquery parent/children documentation to target the textarea.
-        We need to be able to save this data so we can push it to local storage. */
     userInput = $(this).parent("div").children("textarea").val();
     console.log(userInput);
-/*    What? How Why?
-        The event listener will also record the ID tag with the user input.
-        We will use the same parent/children documentation to target the ID.
-        We need to be able to pair the user text input with it's proper ID tag. */
     userTime = $(this).parent("div").parent().attr("id");
     console.log(userTime);
-/*    What? How? Why?
-        We will take these two captured values and pair them together.
-        Using a key/value array.
-        So we can use this array to save to JSON local storage. */
-    keyVal = {
-        time: userTime,
-        plans: userInput
-    }
-    console.log(keyVal);
-/*  What? How? Why?
+    localStorage.setItem(userTime, userInput)
+    // keyVal = {
+    //     time: userTime,
+    //     plans: userInput
+    // }
+    // console.log(keyVal);
+    // userPlans = JSON.parse(localStorage.getItem("plans"));
+    // if (userPlans === null) {
+    //     localStorage.setItem("plans", JSON.stringify([{ time: userTime, plans: userInput }]));
+    // }
+    // else {
+    //     userPlans.push(keyVal);
+    //     localStorage.setItem("plans", JSON.stringify(userPlans));
+    // }
+});
 
-*/
-    userPlans = JSON.parse(localStorage.getItem("plans"));
-    if (userPlans === null) {
-        localStorage.setItem("plans", JSON.stringify([{ time: userTime, plans: userInput }]));
-    }
-    else {
-        userPlans.push(keyVal);
-        localStorage.setItem("plans", JSON.stringify(userPlans));
-    }
-    // console.log(userPlans)
-})
+if (localStorage.getItem("9") !== null) {
+    $("9").parent("div").children("textarea").text(localStorage.getItem("9"));
+}
+
+// $(".saveBtn").click(function() {
+//     var value = $(this).siblings(".text-box").val();
+//     // console.log(userInput);
+//     var key = $(this).siblings(".time").text();
+//     // console.log(userTime);
+//     localStorage.setItem(key, value);
+//     console.log(value);
+//     console.log(key);
+// })
+
+// $(".saveBtn").click(function () {
+//     var value = $(this).siblings(".textentry").val();
+//     var key = $(this).siblings(".time").text();
+//     localStorage.setItem(key, value);
+//     console.log(key);
+//     console.log(value);
+// })
